8ff5787822f4eb97552defb19d139fbbfe1ac95a
Andrii Gulevatenko <otobrazheniye@gmail.com>
2026-05-28 17:08:35 +0200

    preparation for start develop

---
+++ backend/aibusiness/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(max_length=140, unique=True, blank=True)
+
+    short_description = models.CharField(max_length=255)
+    full_description = models.TextField()
+
+    icon_name = models.CharField(max_length=80, blank=True)
+    order = models.PositiveIntegerField(default=0)
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['order', 'title']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
