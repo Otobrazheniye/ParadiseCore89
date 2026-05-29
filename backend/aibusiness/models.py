684fb830a9196527d0e3f99d5e6c21edaa117a41
Andrii Gulevatenko <otobrazheniye@gmail.com>
2026-05-29 14:19:57 +0200

    analyze the model complited

--- backend/aibusiness/models.py
+++ backend/aibusiness/models.py
@@ -4,15 +4,26 @@ from django.utils.text import slugify
 
 class Service(models.Model):
     title = models.CharField(max_length=120, blank=False)
-    slug = models.SlugField("aibusiness", max_length=140, unique=True, blank=True)
-    short_description = models.CharField(max_lenght=255, blank = False)
+    slug = models.SlugField(max_length=140, unique=True, blank=True)
+    short_description = models.CharField(max_length=255, blank = False)
     full_description = models.TextField(blank=False)
     icon_name = models.CharField(max_length=80,blank=True)
     order = models.PositiveIntegerField(default=0)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DataTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
+    
+    class Meta:
+        ordering = ['order','title']
 
 
+    def save(self,*args,**kwargs):
+        if not self.slug:
+           self.slug = slugify(self.title)
+        super().save(*args,**kwargs)
+
+
+    def __str__(self):
+        return self.title
 
