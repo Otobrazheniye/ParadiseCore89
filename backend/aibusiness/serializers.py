c654cc2913ab73d9d374284e67e74012dd7d9698
Andrii Gulevatenko <otobrazheniye@gmail.com>
2026-05-30 16:48:14 +0200

    first serializer complete

--- backend/aibusiness/serializers.py
+++ backend/aibusiness/serializers.py
@@ -5,11 +5,18 @@ class ServiceListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         fields = (
-            'id',
-            'title',
-            'slug',
-            'short_description',
-            'icon_name',
-            'order',
+            'id', 'title',
+            'slug', 'short_description',
+            'icon_name', 'order',
         )
 
+class ServiceDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = (
+            'id', 'title',
+            'slug', 'short_description',
+            'full_description', 'icon_name', 
+            'order', 'created_at',
+            'updated_at'
+        )
