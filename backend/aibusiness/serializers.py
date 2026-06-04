207051114ab3b4245e67ea8a14b4f90c0dab6625
Andrii Gulevatenko <otobrazheniye@gmail.com>
2026-06-04 12:51:46 +0200

    Contact Request Serializer complete, add small validation

--- backend/aibusiness/serializers.py
+++ backend/aibusiness/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Service
+from .models import Service, ContactRequest
 
 
 class ServiceListSerializer(serializers.ModelSerializer):
@@ -22,3 +22,28 @@ class ServiceDetailSerializer(serializers.ModelSerializer):
             'order', 'created_at',
             'updated_at'
         )
+
+class ContactRequestCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactRequest
+        fields = (
+        'id',
+        'name', 'email',
+        'company', 'service_interest',
+        'message', 'created_at',    
+        )
+
+        read_only_fields = (
+            'id', 'created_at',
+        )
+        
+    def validate_name(self, value):
+        if len(value.strip()) < 2:
+            raise serializers.ValidationError("Name must be at least 3 symbols")
+        return value 
+        
+    def validate_message(self, value):
+        if len(value.strip()) < 10:
+            raise serializers.ValidationError("message must be at least 11 symbol")
+        return value
+        
