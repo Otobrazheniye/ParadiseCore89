1342f2bb5fe44999923dd16c8fba37f5732acd7d
Andrii Gulevatenko <otobrazheniye@gmail.com>
2026-06-07 16:54:02 +0200

    Review Urls created

--- backend/aibusiness/urls.py
+++ backend/aibusiness/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import ServiceViewSet, ContactRequestViewSet
+from .views import ServiceViewSet, ContactRequestViewSet, ReviewViewSet
 
 
 router = DefaultRouter()
 
 
-router.register('services', ServiceViewSet, basename='service')
-router.register('contact-request', ContactRequestViewSet, basename='contact-request')
+router.register("services", ServiceViewSet, basename="service")
+router.register("contact-request", ContactRequestViewSet, basename="contact-request")
+router.register("reviews", ReviewViewSet, basename="review")
 
 
 urlpatterns = [
