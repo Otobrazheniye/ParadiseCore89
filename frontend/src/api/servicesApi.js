b622eab55e6317d05bd0f8072c3f6924c784ce40
Andrii Gulevatenko <otobrazheniye@gmail.com>
2026-06-07 21:48:39 +0200

    prepared frontend for backend

--- frontend/src/api/servicesApi.js
+++ frontend/src/api/servicesApi.js
@@ -1,12 +1,12 @@
-import { apiClient } from './client.js';
+import { apiClient } from "./client.js";
 
 export async function getServices(){
-    const response = await apiClient.get('/services/');
+    const response = await apiClient.get("/services/")
     return response.data
 }
 
 
 export async function getServiceBySlug(slug) {
-  const response = await apiClient.get(`/services/${slug}/`);
-  return response.data;
+  const response = await apiClient.get(`/services/${slug}/`)
+  return response.data
 }
